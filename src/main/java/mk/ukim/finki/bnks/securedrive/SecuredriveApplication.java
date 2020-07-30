b49735d6426e17c18f3564fbd312ecdc5d08119d
Teodor Angelovski <angelovski.teo@outlook.com>
2020-07-30 19:15:34 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/bnks/securedrive/SecuredriveApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.bnks.securedrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuredriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/bnks/securedrive/SecuredriveApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.bnks.securedrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
