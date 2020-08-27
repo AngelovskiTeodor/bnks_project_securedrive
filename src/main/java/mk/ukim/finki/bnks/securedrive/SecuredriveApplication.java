aaffb741f25ed34d162e9e1974ee53407f47d862
Teodor Angelovski <angelovski.teo@outlook.com>
2020-08-27 12:07:08 +0200

    Fixed bug about SpringSecurity (added PasswordEncoder Bean)

--- src/main/java/mk/ukim/finki/bnks/securedrive/SecuredriveApplication.java
+++ src/main/java/mk/ukim/finki/bnks/securedrive/SecuredriveApplication.java
@@ -2,12 +2,20 @@ package mk.ukim.finki.bnks.securedrive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SecuredriveApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecuredriveApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 
 }
